40e12377ac67c7b2229edd5c6d0e9a97510142f4
Sitdhibong Laokok <sitdhibong@gmail.com>
2022-08-22 16:01:21 +0700

    Reduce tables

--- week1.data-modeling-1/model.py
+++ week1.data-modeling-1/model.py
@@ -1,6 +1,6 @@
 from email.policy import default
 from sqlalchemy import BigInteger, Boolean, Column, ForeignKey, Integer
-from sqlalchemy import Column, BigInteger, String, Boolean, TIMESTAMP, text, Date, Time
+from sqlalchemy import Column, BigInteger, String, Boolean, TIMESTAMP, Text, Date, Time
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -9,7 +9,7 @@ default_int = -9999
 default_str = 'zzzz'
 
 class Datetime(Base):
-    __tablename__ = 'dim_datetime'
+    __tablename__ = 'datetime'
 
     id = Column(BigInteger, primary_key=True)
     the_date = Column(Date)
@@ -22,7 +22,7 @@ class Datetime(Base):
     the_second = Column(Integer)
 
 class Organize(Base):
-    __tablename__ = 'dim_organize'
+    __tablename__ = 'organize'
 
     id = Column(BigInteger, primary_key=True)
     org_id = Column(BigInteger, default=default_int)
@@ -32,7 +32,7 @@ class Organize(Base):
     avatar_id = Column(String, default=default_str)
 
 class User(Base):
-    __tablename__ = 'dim_user'
+    __tablename__ = 'user'
 
     id = Column(BigInteger, primary_key=True)
     user_id = Column(BigInteger, default=default_int)
@@ -52,116 +52,37 @@ class User(Base):
     repos_url = Column(String, default=default_str)
     events_url = Column(String, default=default_str)
     received_events_url = Column(String, default=default_str)
-    type = Column(String, default=default_str)
+    user_type = Column(String, default=default_str)
     site_admin = Column(String, default=default_str)
 
-class Label(Base):
-    __tablename__ = 'dim_label'
-
-    id = Column(BigInteger, primary_key=True)
-    label_id = Column(BigInteger, default=default_int)
-    node_id = Column(String, default=default_str)
-    url = Column(String, default=default_str)
-    name = Column(String, default=default_str)
-    color = Column(String, default=default_str)
-    default_value = Column(Integer, default=default_int)
-    description = Column(String, default=default_str)
-
 class Repository(Base):
-    __tablename__ = 'dim_repo'
+    __tablename__ = 'repo'
 
     id = Column(BigInteger, primary_key=True)
     repo_id = Column(BigInteger, default=default_str)
     node_id = Column(String, default=default_str)
     name = Column(String, default=default_str)
-    full_name = Column(String, default=default_str)
-    private = Column(Integer, default=default_int)
-    html_url = Column(String, default=default_str)
-    description = Column(String, default=default_str)
-    fork = Column(Integer, default=default_int)
-    url = Column(String, default=default_str)
-    forks_url = Column(String, default=default_str)
-    keys_url = Column(String, default=default_str)
-    collaborators_url = Column(String, default=default_str)
-    teams_url = Column(String, default=default_str)
-    hooks_url = Column(String, default=default_str)
-    issue_events_url = Column(String, default=default_str)
-    events_url = Column(String, default=default_str)
-    assignees_url = Column(String, default=default_str)
-    branches_url = Column(String, default=default_str)
-    tags_url = Column(String, default=default_str)
-    blobs_url = Column(String, default=default_str)
-    git_tags_url = Column(String, default=default_str)
-    git_refs_url = Column(String, default=default_str)
-
-class Comment(Base):
-    __tablename__ = 'dim_comment'
-
-    id = Column(BigInteger, primary_key=True)
-    comment_id = Column(BigInteger, nullable=True)
-    url = Column(String, default=default_str)
-    html_url = Column(String, default=default_str)
-    issue_url = Column(String, default=default_str)
-    node_id = Column(String, default=default_str)
-    author_association = Column(String, default=default_str)
-    body = Column(String, default=default_str)
-    performed_via_github_app = Column(String, default=default_str)
-
-    created_date_id = Column(BigInteger, ForeignKey('dim_datetime.id'), nullable=True)
-    created_at = relationship('Datetime', back_populates='dim_comment')
-
-    updated_date_id = Column(BigInteger, ForeignKey('dim_datetime.id'), nullable=True)
-    updated_at = relationship('Datetime', back_populates='dim_comment')
-    
-    # reaction = relationship('Reaction', back_populates='comment')
-
-class Reaction(Base):
-    __tablename__ = 'dim_reaction'
-
-    id = Column(BigInteger, primary_key=True)
-    reaction_id = Column(BigInteger, default=default_int)
-    url = Column(String, default=default_str)
-    total_count = Column(Integer, default=default_int)
-    plus = Column(Integer, default=default_int)
-    minus = Column(Integer, default=default_int)
-    laugh = Column(Integer, default=default_int)
-    hooray = Column(Integer, default=default_int)
-    confused = Column(Integer, default=default_int)
-    hearth = Column(Integer, default=default_int)
-    rocket = Column(Integer, default=default_int)
-    eyes = Column(Integer, default=default_int)
-
-class Commit(Base):
-    __tablename__ = 'dim_commit'
-
-    id = Column(BigInteger, primary_key=True)
-    commit_id = Column(String, default=default_str)
-    message = Column(String, default=default_str)
-    distinct = Column(Integer, default=default_int)
-    url = Column(String, default=default_str)
 
 class Event(Base):
-    __tablename__ = 'fact_event'
+    __tablename__ = 'event'
 
     id = Column(String, primary_key=True)
     event_id = Column(String, default=default_str)
-    type = Column(String, default=default_str)
-    actor_id = Column(BigInteger, ForeignKey('dim_user.id'), nullable=True)
-    repo_id = Column(BigInteger, ForeignKey('dim_repo.id'), nullable=True)
-    org_id = Column(BigInteger, ForeignKey('dim_organize.id'), nullable=True)
+    event_type = Column(String, default=default_str)
+    actor_id = Column(BigInteger, ForeignKey('user.id'), nullable=True)
+    repo_id = Column(BigInteger, ForeignKey('repo.id'), nullable=True)
+    org_id = Column(BigInteger, ForeignKey('organize.id'), nullable=True)
+
+    payload = Column(Text, default=default_str)
 
     date_id = Column(BigInteger, default=default_int)
     created_at = Column(String)
 
     public = Column(Integer, default=default_int)
 
-    repository = relationship('Repository', back_populates='fact_event')
-    user = relationship('User', back_populates='fact_event')
-    organize = relationship('Organize', back_populates='fact_event')
+    repository = relationship('Repository', back_populates='event')
+    user = relationship('User', back_populates='event')
+    organize = relationship('Organize', back_populates='event')
 
-    date_id = relationship('Datetime', back_populates='fact_event')
-
-    # created_at = Column(
-    #     TIMESTAMP, 
-    #     server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
-    # )
+    date_id = relationship('Datetime', back_populates='event')
+    created_at = Column(String, nullable=True)
