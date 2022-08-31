0f9bf35306b25c17f7aa9225093f92459d4d59d6
Sitdhibong Laokok <sitdhibong@gmail.com>
2022-08-31 16:54:36 +0700

    Test code for connect to db

--- week2.data-modeling-2/github-eventscapture.py
+++ week2.data-modeling-2/github-eventscapture.py
@@ -2,5 +2,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import pymongo as mg
 
-print(os.getenv('DB_NAME'))
+cont_str = 'mongodb://{u}:{p}@{h}:27017/'.format(
+    u=os.getenv('DB_ROOT_USERNAME', 'user'),
+    p=os.getenv('DB_ROOT_PASSWORD', 'password'),
+    h=os.getenv('DB_HOST', 'host_name'),
+)
+
+client = mg.MongoClient(
+    cont_str,
+    serverSelectionTimeoutMS=5_000
+)
+
+db = client['data']
+
+user = db['users']
+user_id = user.insert_one({
+    'user': 'johnd',
+    'text': 'hello',
+}).inserted_id
+
+print(user_id)
